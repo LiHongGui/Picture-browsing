79b8b12d0bbac736800d080b372c13de7ce788df
LiHongGui <lihonggui1988@outlook.com>
2016-09-19 15:08:43 +0800

    手动创建字典---数组

---
+++ 图片浏览器(第二遍)(字典---数组) /图片浏览器(第二遍)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片浏览器(第二遍)
+//
+//  Created by MichaelLi on 16/9/19.
+//  Copyright © 2016年 手持POS机. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
