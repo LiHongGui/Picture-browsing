9a2f0586467d74c93274a3c6addd1ee5bc041121
LiHongGui <lihonggui1988@outlook.com>
2016-09-30 16:47:33 +0800

    no message

--- 图片浏览(播放动画---优化) /图片浏览(播放动画)/ViewController.m
+++ 图片浏览(播放动画---优化) /图片浏览(播放动画)/ViewController.m
@@ -16,7 +16,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    [super viewDidLoad];  
     // Do any additional setup after loading the view, typically from a nib.
 }
 -(void)imageAnimationString:(NSString *)string andWithNumber:(int)number
